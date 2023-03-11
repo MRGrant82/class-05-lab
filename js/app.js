3bef97cd208a47b1dec04e345cc01191eb85db99
Michael Grant <serus12345@gmail.com>
2023-03-10 20:41:36 -0500

    join function to concatinate (sp?)

--- js/app.js
+++ js/app.js
@@ -89,24 +89,47 @@ testSumAndMultiply(4,7,5);
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array 
+where the first element is the sum of the numbers in the array, and the second element is a string that 
+EXACTLY follows this example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function 
+that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use 
+the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, 
+uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
+function sumArray(sumArr) {
+  // Reuse the function from above
+  function sum(a, b) {
+    return a + b;
+  }
+  // initializing
+  let total = 0;
 
-function sumArray(sumArr) { //eslint-disable-line
-
+  for (let i = 0; i < sumArr.length; i++) {
+    // This iterates through each element of the array above and will call my sum function to add them together
+    total = sum(total, sumArr[i]);
+  }
+    // We still use the `` and use join which concatenates the array into a string
+  let message = `${sumArr.join(',')} was passed in as an array of numbers, and ${total} is their sum.`;
+  // Why is the console printing twice?
+  console.log(message);
+  return [total, message];
+  
 }
+
+
+  
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
